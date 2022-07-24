160d91536cab2818334dc8509ef37bfcb64ae14d
callejon1021 <andreikrz1021@gmail.com>
2022-07-24 19:39:35 +0300

    aaa

---
+++ lesson1/main.js
@@ -0,0 +1,81 @@
+
+
+let str1 = 'hello '
+console.log(str1)
+document.write(str1)
+// alert(str1)
+let str2 = 'owu '
+console.log(str2)
+document.write(str2)
+// alert(str2)
+let str3 = 'com '
+console.log(str3)
+document.write(str3)
+// alert(str3)
+let str4 = 'ua'
+console.log(str4)
+document.write(str4)
+// alert(str4)
+
+let num1 = 1;
+console.log(num1)
+document.write(num1)
+// alert(num1)
+
+let num2 = 10;
+console.log(num2)
+document.write(num2)
+// alert(num2)
+let num3 = -999;
+console.log(num3)
+document.write(num3)
+// alert(num3)
+let num4 = 123;
+console.log(num4)
+document.write(num4)
+// alert(num4)
+let num5 = 3.14;
+console.log(num5)
+document.write(num5)
+// alert(num5)
+let num6 = 2.7;
+console.log(num6)
+document.write(num6)
+// alert(num6)
+let num7 = 16;
+console.log(num7)
+document.write(num7)
+// alert(num7)
+
+let bool1 = true;
+console.log(bool1)
+document.write(bool1)
+// alert(bool1)
+let bool2 = false;
+console.log(bool2)
+document.write(bool2)
+// alert(bool2)
+
+let firstname = 'Andrii'
+console.log(firstname)
+document.write(firstname)
+let middle = 'Oleksandrovych'
+console.log(middle)
+document.write(middle)
+let second = 'Hlazun'
+console.log(second)
+document.write(second)
+let person = `${firstname} ${middle} ${second}`;
+console.log(person)
+
+let a = typeof 100;
+let b = typeof '100'
+let c = typeof true;
+console.log(a,b,c)
+
+let imputname = prompt('Andrii')
+console.log(imputname)
+let imputmiddle = prompt('Hlazun')
+console.log(imputmiddle)
+let imputage = prompt('26 years')
+console.log(imputage)
